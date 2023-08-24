b6243f7278e473ceac0a6ec7ecd73324b8a86503
Yorgo Petsas <yorgopecas@gmail.com>
2023-08-24 09:24:23 +0200

    Header Created

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,1 +1,28 @@
-// Write all the code here
+import React from 'react';
+import { IoFastFoodOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+import { FiShoppingCart } from 'react-icons/fi';
+import DropdownMenu from './DropdownMenu';
+
+const Header = ({ isAuthenticated = false }) => {
+  return (
+    <nav>
+		<motion.div initial={{ x: "-100%" }} whileInView={{ x: 0 }}>
+			<IoFastFoodOutline />
+		</motion.div>
+
+      <div>
+        <Link to='/'>Inicio</Link>
+        <Link to='/contact'>Contacto</Link>
+        <Link to='/about'>Quiénes Somos</Link>
+        <Link to='/cart'>
+          <FiShoppingCart />
+        </Link>
+
+        <DropdownMenu />
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
