28c70db764875cb3389e2f4528d785138db48455
Yorgo Petsas <yorgopecas@gmail.com>
2023-08-24 09:33:22 +0200

    Menu Created

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,1 +1,46 @@
-// Write all the code here
+import React from "react";
+import MenuCard from "./MenuCard";
+import burger1 from "../../assets/burger1.png";
+import burger2 from "../../assets/burger2.png";
+import burger3 from "../../assets/burger3.png";
+
+
+const Menu = () => {
+	const addToCartHandler = (itemNum) => {};
+	
+		return (
+			<section id="menu">
+				< h1> MENÚ</h1>
+				
+				<div>
+					<MenuCard
+						itemNum={1}
+						burgerSrc={burger1}
+						price={3,30}
+						title="Hamburguesa con queso"
+						handler={addToCartHandler}
+						delay={0.1}
+					/>
+					<MenuCard
+						itemNum={2
+						}
+						burgerSrc={burger2}
+						price={4,50}
+						title="Hamburguesa vegetariana con queso"
+						delay={0.5}
+						handler={addToCartHandler}
+					/>
+					<MenuCard
+						itemNum={3}
+						burgerSrc={burger3}
+						price={6,10}
+						title="Hamburguesa con queso y patatas fritas"
+						delay={0.8}
+						handler={addToCartHandler}
+					/>
+				</div>
+			</section>
+		);
+	};
+
+export default Menu;
