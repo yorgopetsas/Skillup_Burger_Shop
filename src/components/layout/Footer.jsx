9e53118889f1fa16dd57d57aa209da643e66ae27
Yorgo Petsas <yorgopecas@gmail.com>
2023-08-24 09:29:06 +0200

    Footer Created

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,32 @@
-// Write all the code here
+import React from 'react';
+import { AiFillInstagram, AiFillYoutube } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <footer>
+		<div>
+			<h2>Hamburguesería</h2>
+			
+			<p>Intentamos darte el mejor sabor posible.</p>
+			<br />
+			
+			<em>Prestamos atención a tus comentarios.</em>
+			
+			<strong>Todos los derechos reservados @hamburgueseria</strong>
+		</div>
+		
+        <aside>
+            <h4>Síguenos</h4>
+
+			<a href="https://youtube.com/xyz">
+                <AiFillYoutube />
+            </a>
+            <a href="https://instagram.com/xyz">
+                <AiFillInstagram />
+            </a>
+		</aside>
+    </footer>
+  );
+};
+
+export default Footer;
